eac75e7f25f8fe757f371de218516b0629f32990
Sssskkyla <Skylasrobertson@gmail.com>
2023-09-25 04:12:00 +0000

    Add hello

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-
+config.hosts << "a0ddc86ff9a34d0ca8ac617157d40c55.vfs.cloud9.us-east-2.amazonaws.com"
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
